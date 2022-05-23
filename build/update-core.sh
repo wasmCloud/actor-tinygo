93b285ec0617f0cf200c6b63c3f33930c6f070a0
stevelr <steve@cosmonic.com>
2022-05-22 19:27:17 -0700

    script to update core.go and model.go from latest smithy files
    Signed-off-by: stevelr <steve@cosmonic.com>

---
+++ build/update-core.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Update core.go and model.go from latest smithy definitions
+
+# Usage:  (run from top-level 'actor-tinygo' folder)
+#   build/update-core.sh
+
+WASH=wash
+#WASH=../wash/target/debug/wash
+
+$WASH gen -c build/codegen.toml
+
