f416f45af3128d3b1a6735c1fe93d5e227d8a090
stevelr <steve@cosmonic.com>
2022-05-24 22:04:18 -0700

    add serialization for Timestamp
    Signed-off-by: stevelr <steve@cosmonic.com>

--- rpc.go
+++ rpc.go
@@ -1,15 +1,20 @@
 package actor
 
-/// Message contains an rpc operation name and a binary (serialized) payload
+import (
+	cbor "github.com/wasmcloud/tinygo-cbor"
+	msgpack "github.com/wasmcloud/tinygo-msgpack"
+)
+
+// Message contains an rpc operation name and a binary (serialized) payload
 type Message struct {
 	Method string
 	Arg    []byte
 }
 
-/// Context object passed through rpc methods
+// Context object passed through rpc methods
 type Context struct{}
 
-/// Transport interface used to send rpc messages over some connection
+// Transport interface used to send rpc messages over some connection
 type Transport struct {
 	binding   string
 	namespace string
@@ -37,18 +42,18 @@ func (t *Transport) Send(ctx *Context, msg Message) ([]byte, error) {
 	return r, ok
 }
 
-/// RpcError is an error type emitted by the rpc infrastructure
+// RpcError is an error type emitted by the rpc infrastructure
 type RpcError struct {
 	kind    string
 	message string
 }
 
-/// NewRpcError constructs and RpcError
+// NewRpcError constructs an RpcError
 func NewRpcError(kind string, message string) *RpcError {
 	return &RpcError{kind: kind, message: message}
 }
 
-/// Error is RpcError's implementation of the error interface
+// Error is RpcError's implementation of the error interface
 func (e *RpcError) Error() string {
 	return e.message
 }
@@ -57,7 +62,30 @@ func (e *RpcError) Error() string {
 // additional data types used in Smithy models
 //
 
-/// Timestamp identifies an instant in time
+// Document is an 'any' type interface and is not currently implemented for TinyGo actors
+type Document struct{}
+
+func (o *Document) MEncode(encoder msgpack.Writer) error {
+	return nil
+}
+func (o *Document) CEncode(encoder cbor.Writer) error {
+	return nil
+}
+func MDecodeDocument(d *msgpack.Decoder) Document {
+	return Document{}
+}
+func CDecodeDocument(d *cbor.Decoder) Document {
+	return Document{}
+}
+
+// ConsoleLog sends log message to host console.
+// For internal use - Actors should use the logging capability provider for logging
+// Deprecated - support for this function may go away
+func ConsoleLog(msg string) {
+	consoleLog(stringToPointer(msg), uint32(len(msg)))
+}
+
+// Timestamp identifies an instant in time
 type Timestamp struct {
 	// the number of non-leap seconds since unix epoch in UTC
 	Sec int64
@@ -65,12 +93,88 @@ type Timestamp struct {
 	Nsec uint32
 }
 
-/// Document is an 'any' type interface and is not currently implemented for TinyGo actors
-type Document struct{}
+// MEncode serializes a Timestamp using msgpack
+func (o *Timestamp) MEncode(encoder msgpack.Writer) error {
+	encoder.WriteMapSize(2)
+	encoder.WriteString("sec")
+	encoder.WriteInt64(o.Sec)
+	encoder.WriteString("nsec")
+	encoder.WriteUint32(o.Nsec)
+	return encoder.CheckError()
+}
 
-// ConsoleLog sends log message to host console.
-// For internal use - Actors should use the logging capability provider for logging
-// Deprecated - support for this function may go away
-func ConsoleLog(msg string) {
-	consoleLog(stringToPointer(msg), uint32(len(msg)))
+// MDecodeTimestamp deserializes a Timestamp using msgpack
+func MDecodeTimestamp(d *msgpack.Decoder) (Timestamp, error) {
+	var val Timestamp
+	isNil, err := d.IsNextNil()
+	if err != nil || isNil {
+		return val, err
+	}
+	size, err := d.ReadMapSize()
+	if err != nil {
+		return val, err
+	}
+	for i := uint32(0); i < size; i++ {
+		field, err := d.ReadString()
+		if err != nil {
+			return val, err
+		}
+		switch field {
+		case "sec":
+			val.Sec, err = d.ReadInt64()
+		case "nsec":
+			val.Nsec, err = d.ReadUint32()
+		default:
+			err = d.Skip()
+		}
+		if err != nil {
+			return val, err
+		}
+	}
+	return val, nil
+
+}
+
+// CEncode serializes a Timestamp using cbor
+func (o *Timestamp) CEncode(encoder cbor.Writer) error {
+	encoder.WriteMapSize(2)
+	encoder.WriteString("sec")
+	encoder.WriteInt64(o.Sec)
+	encoder.WriteString("nsec")
+	encoder.WriteUint32(o.Nsec)
+	return encoder.CheckError()
+}
+
+// CDecodeTimestamp deserializes a Timestamp using cbor
+func CDecodeTimestamp(d *cbor.Decoder) (Timestamp, error) {
+	var val Timestamp
+	isNil, err := d.IsNextNil()
+	if err != nil || isNil {
+		return val, err
+	}
+	size, indef, err := d.ReadMapSize()
+	if err != nil && indef {
+		err = cbor.NewReadError("indefinite maps not supported")
+	}
+	if err != nil {
+		return val, err
+	}
+	for i := uint32(0); i < size; i++ {
+		field, err := d.ReadString()
+		if err != nil {
+			return val, err
+		}
+		switch field {
+		case "sec":
+			val.Sec, err = d.ReadInt64()
+		case "nsec":
+			val.Nsec, err = d.ReadUint32()
+		default:
+			err = d.Skip()
+		}
+		if err != nil {
+			return val, err
+		}
+	}
+	return val, nil
 }
